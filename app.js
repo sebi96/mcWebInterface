85f3f315f6e8e95017a91fd203f6a40f99749c6d
Sebi <sebastian.delles@web.de>
2012-12-27 21:12:43 +0100

    startet using jade... not done so far!

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 var SECURITYTOKKEN = "aaa"; //at least 128bit strong key
 var port = 8000;
+var title = "Servertitle";
+var url = "IP:Port"; // eg 127.0.0.1:8000
+var admin = "admin" // Name of the server admin
 
 /** END SETTINGS **/
 
@@ -19,6 +22,7 @@ var app = express();
 
 app.configure(function(){
   app.set('port', port);
+  app.set('views', __dirname);
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.bodyParser());
@@ -54,15 +58,25 @@ app.get('/' + SECURITYTOKKEN + '/restart/?', function(req, res){
 
 
 app.get('/' + SECURITYTOKKEN + '/?', function(req, res){
-     fs.readFile('./file.html', function(error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        }
-        else {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(content, 'utf-8');        }
-    }); 
+  fs.readFile('./file.html', function(error, content) {
+    if (error) {
+        res.writeHead(500);
+        res.end();
+    }
+    else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');        }
+  }); 
+});
+
+
+app.get('/' + SECURITYTOKKEN + '/d?', function(req, res){
+     /*res.render("template", {
+      title: "TITLE",
+      admin: "admin"  
+     });*/
+  res.render ("template")
+
 });
 
 
