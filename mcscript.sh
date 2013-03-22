ec636f6d956ef53bf36a2483b8e10832e95e5120
sebastian <email-to-me@qip.ru>
2013-03-22 15:58:46 +0100

    shellscript repaired

--- mcscript.sh
+++ mcscript.sh
@@ -3,20 +3,20 @@
 # Get the script from http://www.minecraftwiki.net/wiki/Tutorials/Server_startup_script#Script
 
 
-#!binbash
-# etcinit.dminecraft
+#!/bin/bash
+# /etc/init.d/minecraft
 # version 0.3.9 2012-08-13 (YYYY-MM-DD)
 
 ### BEGIN INIT INFO
-# Provides   minecraft
-# Required-Start $local_fs $remote_fs
-# Required-Stop  $local_fs $remote_fs
-# Should-Start   $network
-# Should-Stop    $network
-# Default-Start  2 3 4 5
-# Default-Stop   0 1 6
-# Short-Description    Minecraft server
-# Description    Starts the minecraft server
+# Provides:   minecraft
+# Required-Start: $local_fs $remote_fs
+# Required-Stop:  $local_fs $remote_fs
+# Should-Start:   $network
+# Should-Stop:    $network
+# Default-Start:  2 3 4 5
+# Default-Stop:   0 1 6
+# Short-Description:    Minecraft server
+# Description:    Starts the minecraft server
 ### END INIT INFO
 
 #Settings
@@ -24,106 +24,106 @@ SERVICE='minecraft_server.jar'
 OPTIONS='nogui'
 USERNAME='minecraft'
 WORLD='world'
-MCPATH='homeminecraftminecraft'
-BACKUPPATH='mediaremote.shareminecraft.backup'
+MCPATH='/home/minecraft/minecraft'
+BACKUPPATH='/media/remote.share/minecraft.backup'
 MAXHEAP=2048
 MINHEAP=1024
 HISTORY=1024
 CPU_COUNT=1
-INVOCATION=java -Xmx${MAXHEAP}M -Xms${MINHEAP}M -XX+UseConcMarkSweepGC 
--XX+CMSIncrementalPacing -XXParallelGCThreads=$CPU_COUNT -XX+AggressiveOpts 
--jar $SERVICE $OPTIONS
+INVOCATION="java -Xmx${MAXHEAP}M -Xms${MINHEAP}M -XX:+UseConcMarkSweepGC \
+-XX:+CMSIncrementalPacing -XX:ParallelGCThreads=$CPU_COUNT -XX:+AggressiveOpts \
+-jar $SERVICE $OPTIONS"
 
 ME=`whoami`
 as_user() {
   if [ $ME == $USERNAME ] ; then
-    bash -c $1
+    bash -c "$1"
   else
-    su - $USERNAME -c $1
+    su - $USERNAME -c "$1"
   fi
 }
 
 mc_start() {
-  if  pgrep -u $USERNAME -f $SERVICE  devnull
+  if  pgrep -u $USERNAME -f $SERVICE > /dev/null
   then
-    echo $SERVICE is already running!
+    echo "$SERVICE is already running!"
   else
-    echo Starting $SERVICE...
+    echo "Starting $SERVICE..."
     cd $MCPATH
-    as_user cd $MCPATH && screen -h $HISTORY -dmS minecraft $INVOCATION
+    as_user "cd $MCPATH && screen -h $HISTORY -dmS minecraft $INVOCATION"
     sleep 7
-    if pgrep -u $USERNAME -f $SERVICE  devnull
+    if pgrep -u $USERNAME -f $SERVICE > /dev/null
     then
-      echo $SERVICE is now running.
+      echo "$SERVICE is now running."
     else
-      echo Error! Could not start $SERVICE!
+      echo "Error! Could not start $SERVICE!"
     fi
   fi
 }
 
 mc_saveoff() {
-  if pgrep -u $USERNAME -f $SERVICE  devnull
+  if pgrep -u $USERNAME -f $SERVICE > /dev/null
   then
-    echo $SERVICE is running... suspending saves
-    as_user screen -p 0 -S minecraft -X eval 'stuff say SERVER BACKUP STARTING. Server going readonly...015'
-    as_user screen -p 0 -S minecraft -X eval 'stuff save-off015'
-    as_user screen -p 0 -S minecraft -X eval 'stuff save-all015'
+    echo "$SERVICE is running... suspending saves"
+    as_user "screen -p 0 -S minecraft -X eval 'stuff \"say SERVER BACKUP STARTING. Server going readonly...\"\015'"
+    as_user "screen -p 0 -S minecraft -X eval 'stuff \"save-off\"\015'"
+    as_user "screen -p 0 -S minecraft -X eval 'stuff \"save-all\"\015'"
     sync
     sleep 10
   else
-    echo $SERVICE is not running. Not suspending saves.
+    echo "$SERVICE is not running. Not suspending saves."
   fi
 }
 
 mc_saveon() {
-  if pgrep -u $USERNAME -f $SERVICE  devnull
+  if pgrep -u $USERNAME -f $SERVICE > /dev/null
   then
-    echo $SERVICE is running... re-enabling saves
-    as_user screen -p 0 -S minecraft -X eval 'stuff save-on015'
-    as_user screen -p 0 -S minecraft -X eval 'stuff say SERVER BACKUP ENDED. Server going read-write...015'
+    echo "$SERVICE is running... re-enabling saves"
+    as_user "screen -p 0 -S minecraft -X eval 'stuff \"save-on\"\015'"
+    as_user "screen -p 0 -S minecraft -X eval 'stuff \"say SERVER BACKUP ENDED. Server going read-write...\"\015'"
   else
-    echo $SERVICE is not running. Not resuming saves.
+    echo "$SERVICE is not running. Not resuming saves."
   fi
 }
 
 mc_stop() {
-  if pgrep -u $USERNAME -f $SERVICE  devnull
+  if pgrep -u $USERNAME -f $SERVICE > /dev/null
   then
-    echo Stopping $SERVICE
-    as_user screen -p 0 -S minecraft -X eval 'stuff say SERVER SHUTTING DOWN IN 10 SECONDS. Saving map...015'
-    as_user screen -p 0 -S minecraft -X eval 'stuff save-all015'
+    echo "Stopping $SERVICE"
+    as_user "screen -p 0 -S minecraft -X eval 'stuff \"say SERVER SHUTTING DOWN IN 10 SECONDS. Saving map...\"\015'"
+    as_user "screen -p 0 -S minecraft -X eval 'stuff \"save-all\"\015'"
     sleep 10
-    as_user screen -p 0 -S minecraft -X eval 'stuff stop015'
+    as_user "screen -p 0 -S minecraft -X eval 'stuff \"stop\"\015'"
     sleep 7
   else
-    echo $SERVICE was not running.
+    echo "$SERVICE was not running."
   fi
-  if pgrep -u $USERNAME -f $SERVICE  devnull
+  if pgrep -u $USERNAME -f $SERVICE > /dev/null
   then
-    echo Error! $SERVICE could not be stopped.
+    echo "Error! $SERVICE could not be stopped."
   else
-    echo $SERVICE is stopped.
+    echo "$SERVICE is stopped."
   fi
 }
 
 mc_update() {
-  if pgrep -u $USERNAME -f $SERVICE  devnull
+  if pgrep -u $USERNAME -f $SERVICE > /dev/null
   then
-    echo $SERVICE is running! Will not start update.
+    echo "$SERVICE is running! Will not start update."
   else
-    MC_SERVER_URL=https3.amazonaws.comMinecraftDownloadlauncherminecraft_server.jarv=`date  sed s[^a-zA-Z0-9]_g`
-    as_user cd $MCPATH && wget -q -O $MCPATHminecraft_server.jar.update $MC_SERVER_URL
-    if [ -f $MCPATHminecraft_server.jar.update ]
+    MC_SERVER_URL=http://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar?v=`date | sed "s/[^a-zA-Z0-9]/_/g"`
+    as_user "cd $MCPATH && wget -q -O $MCPATH/minecraft_server.jar.update $MC_SERVER_URL"
+    if [ -f $MCPATH/minecraft_server.jar.update ]
     then
-      if `diff $MCPATH$SERVICE $MCPATHminecraft_server.jar.update devnull`
+      if `diff $MCPATH/$SERVICE $MCPATH/minecraft_server.jar.update >/dev/null`
       then 
-        echo You are already running the latest version of $SERVICE.
+        echo "You are already running the latest version of $SERVICE."
       else
-        as_user mv $MCPATHminecraft_server.jar.update $MCPATH$SERVICE
-        echo Minecraft successfully updated.
+        as_user "mv $MCPATH/minecraft_server.jar.update $MCPATH/$SERVICE"
+        echo "Minecraft successfully updated."
       fi
     else
-      echo Minecraft update could not be downloaded.
+      echo "Minecraft update could not be downloaded."
     fi
   fi
 }
@@ -131,38 +131,38 @@ mc_update() {
 mc_backup() {
    mc_saveoff
    
-   NOW=`date +%Y-%m-%d_%Hh%M`
-   BACKUP_FILE=$BACKUPPATH${WORLD}_${NOW}.tar
-   echo Backing up minecraft world...
-   #as_user cd $MCPATH && cp -r $WORLD $BACKUPPATH${WORLD}_`date +%Y.%m.%d_%H.%M`
-   as_user tar -C $MCPATH -cf $BACKUP_FILE $WORLD
+   NOW=`date "+%Y-%m-%d_%Hh%M"`
+   BACKUP_FILE="$BACKUPPATH/${WORLD}_${NOW}.tar"
+   echo "Backing up minecraft world..."
+   #as_user "cd $MCPATH && cp -r $WORLD $BACKUPPATH/${WORLD}_`date "+%Y.%m.%d_%H.%M"`"
+   as_user "tar -C \"$MCPATH\" -cf \"$BACKUP_FILE\" $WORLD"
 
-   echo Backing up $SERVICE
-   as_user tar -C $MCPATH -rf $BACKUP_FILE $SERVICE
-   #as_user cp $MCPATH$SERVICE $BACKUPPATHminecraft_server_${NOW}.jar
+   echo "Backing up $SERVICE"
+   as_user "tar -C \"$MCPATH\" -rf \"$BACKUP_FILE\" $SERVICE"
+   #as_user "cp \"$MCPATH/$SERVICE\" \"$BACKUPPATH/minecraft_server_${NOW}.jar\""
 
    mc_saveon
 
-   echo Compressing backup...
-   as_user gzip -f $BACKUP_FILE
-   echo Done.
+   echo "Compressing backup..."
+   as_user "gzip -f \"$BACKUP_FILE\""
+   echo "Done."
 }
 
 mc_command() {
-  command=$1;
-  if pgrep -u $USERNAME -f $SERVICE  devnull
+  command="$1";
+  if pgrep -u $USERNAME -f $SERVICE > /dev/null
   then
-    pre_log_len=`wc -l $MCPATHserver.log  awk '{print $1}'`
-    echo $SERVICE is running... executing command
-    as_user screen -p 0 -S minecraft -X eval 'stuff $command015'
+    pre_log_len=`wc -l "$MCPATH/server.log" | awk '{print $1}'`
+    echo "$SERVICE is running... executing command"
+    as_user "screen -p 0 -S minecraft -X eval 'stuff \"$command\"\015'"
     sleep .1 # assumes that the command will run and print to the log file in less than .1 seconds
     # print output
-    tail -n $[`wc -l $MCPATHserver.log  awk '{print $1}'`-$pre_log_len] $MCPATHserver.log
+    tail -n $[`wc -l "$MCPATH/server.log" | awk '{print $1}'`-$pre_log_len] "$MCPATH/server.log"
   fi
 }
 
 #Start-Stop here
-case $1 in
+case "$1" in
   start)
     mc_start
     ;;
@@ -193,16 +193,19 @@ case $1 in
   command)
     if [ $# -gt 1 ]; then
       shift
-      mc_command $
+      mc_command "$*"
     else
-      echo Must specify server command (try 'help')
+      echo "Must specify server command (try 'help'?)"
     fi
     ;;
 
-  )
-  echo Usage $0 {startstopupdatebackupstatusrestartcommand server command}
+  *)
+  echo "Usage: $0 {start|stop|update|backup|status|restart|command \"server command\"}"
   exit 1
   ;;
 esac
 
 exit 0
+
+
+
